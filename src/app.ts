312148797cbe40672cdc2f1ae1992a470e9efd69
seunghunbang <upi21x@gmail.com>
2023-01-24 19:51:17 +0200

    rendering on the screen

--- src/app.ts
+++ src/app.ts
@@ -4,19 +4,36 @@ const form = document.querySelector("form")! as HTMLFormElement;
 
 const addressInput = document.getElementById("address")! as HTMLInputElement;
 const GOOGLE_API_KEY = "AIzaSyBteCxlFUOSX3f6Zj4xUPCYxmmCG56zFhA";
+declare var google: any;
 function searchAddressHandler(event: Event) {
   event.preventDefault();
   const enteredAddress = addressInput.value; // get user input
 
+  type GoogleGeocodingResponse = {
+    results: { geometry: { location: { lat: number; lng: number } } }[];
+    status: "OK" | "ZERO_RESULTS";
+  };
   // send this to Google's API!
   axios
-    .get(
+    .get<GoogleGeocodingResponse>(
       `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
         enteredAddress
       )}&key=${GOOGLE_API_KEY}`
     )
     .then((response) => {
-      console.log(response.data);
+      if (response.data.status !== "OK") {
+        throw new Error("Could not fetch location!");
+      }
+      const coordinates = response.data.results[0].geometry.location;
+
+      const map = new google.maps.Map(
+        document.getElementById("map") as HTMLElement,
+        {
+          center: coordinates,
+          zoom: 16,
+        }
+      );
+      new google.maps.Marker({ position: coordinates, map: map });
     })
     .catch((err) => {
       alert(err.message);
